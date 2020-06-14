16085ed14888d5dbd3d5541ea86f274d70fa3b36
lucaires <lu.caires@outlook.com>
2020-06-13 23:22:56 -0300

    FilterScreen

--- src/screens/Filters/style.js
+++ src/screens/Filters/style.js
@@ -10,7 +10,44 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   filterInfra: {
+    marginTop: 10,
     flexDirection: "row",
+  },
+  item: {
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderColor: "#eee",
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    textAlign: "center",
+  },
+
+  selectedItem: {
+    borderColor: "#34CB79",
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: "Roboto_400Regular",
+    textAlign: "center",
+    fontSize: 13,
+  },
+  classification: {
+    marginBottom: 40,
+  },
+  button: {
+    alignSelf: "center",
+    marginBottom: 30,
+    backgroundColor: "#218080",
+    color: "#fff",
   },
 });
 
